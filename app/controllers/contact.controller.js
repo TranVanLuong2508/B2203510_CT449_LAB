49866949e844049ce04100c9557d290069e02e9b
Tran Van Luong <tranvanluong032020@gmail.com>
2024-09-20 14:45:35 +0700

    update file

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -24,6 +24,8 @@ const ApiError = require("../api-error")
 const ContactService = require("../services/contact.service")
 const MongoDB = require("../utils/mongodb.util")
 
+
+
 exports.create = async (req, res, next ) => {
     if(!req.body?.name) {
         return next (new ApiError(400,"Name can not be empty"))
@@ -39,4 +41,3 @@ exports.create = async (req, res, next ) => {
         )
     }
 }
-
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -1,35 +1,45 @@
-const { ObjectId, ReturnDocument } = require("mongodb")
+const {ObjectId} = require("mongodb")
 
 class ContactService {
-    
-    constructor(client) {
-        this.Contact = client.db().collection("contacts")
-    }
 
-    extractContactData (payload) {
-        const contact ={
-            name: payload.name,
-            email: payload.email,
-            address: payload.address,
-            phone: payload.phone,
-            favorite: payload.favorite,
-        }
+   constructor (client) {
 
-        Object.keys(contact).forEach( (key) =>{
-            contact[key] === undefined && delete contact[key]
-        })
-        return  contact
-    }
+    this.contact= client.db().collection("contacts")
+   }
 
-   async  create(payload) {
-        const contact = this.extractContactData(payload);
-        const result = await this.Contsact.findOneAndUpdate(
-            contact,
-            {$set: {favorite: contact.favorite ===true}},
-            {ReturnDocument: "after", upsert: true}
-        )
-        return result
+   extractContactData(payload) {
+    const contact ={
+        name:payload.name,
+        email:payload.email,
+        address:payload.address,
+        phone: payload.phone,
+        favorite: payload.favorite,
     }
+    Object.keys(contact).forEach(key => {
+        contact[key] === undefined && delete contact[key]
+    })
+    return contact
+   }
+
+   async create (payload) {
+    const contact = this.extractContactData(payload)
+    const result =  await this.contact.findOneAndUpdate(contact, 
+        {$set:{favorite: contact.favorite === true }},
+        {returnDocument:"after", upsert: true}
+    )
+    return result
+   }    
+
+   async find(filter) {
+    const cursor  =  await this.contact.find(filter)
+    return await cursor.toArray()
+   }
+
+   findByName(name) {
+    return  this.find({
+        name: { $regex: new RegExp (new RegExp(name)), $options:"i"}
+    })
+   }
 }
 
 module.exports = ContactService
